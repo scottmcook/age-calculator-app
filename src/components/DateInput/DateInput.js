537519ecdbd6c46d70fb2385f831e864d41c103a
scottmcook <scott.madison.cook@gmail.com>
2023-05-10 12:14:55 -0700

    Add input type number

--- src/components/DateInput/DateInput.js
+++ src/components/DateInput/DateInput.js
@@ -1,10 +1,16 @@
-const DateInput = ({ label, type, id, placeholder }) => {
+const DateInput = ({ label, type, id, placeholder, min, max }) => {
   return (
     <>
       <label htmlFor={id} className="font-semibold capitalize">
         {label}
       </label>
-      <input id={id} type={type} placeholder={placeholder} />
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        min={min}
+        max={max}
+      />
     </>
   );
 };
